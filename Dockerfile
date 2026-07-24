ed5eea3c2e5a54983c581c1765f68c916e02e98e
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-07-24 15:46:47 +0530

    fix: ignore all build files in docker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
-FROM ghcr.io/cloud-cli/node:latest
+FROM ghcr.io/cloud-cli/node:latest AS builder
 
 COPY --chown=1000 . .
 USER 0
-RUN pnpm i && pnpm run build && rm -rf node_modules packages/on/node_modules && pnpm i --prod
-USER 1000
+RUN pnpm i && pnpm run build
+
+FROM ghcr.io/cloud-cli/node:latest
+COPY --from=builder /home/app/packages/on/dist/on.js ./on.mjs
+ENV WORKFLOW_HOST "0.0.0.0"
 ENTRYPOINT [ "node" ]
-CMD [ "./packages/on/dist/on.js" ]
+CMD [ "on.js" ]
--- packages/on/src/cli.ts
+++ packages/on/src/cli.ts
@@ -24,7 +24,7 @@ export function parseCliOptions(argv: string[]): ServerOptions | null {
     args: argv,
     options: {
       port: { type: "string", short: "p", default: process.env.PORT || "11235" },
-      host: { type: "string", short: "H", default: "127.0.0.1" },
+      host: { type: "string", short: "H", default: process.env.WORKFLOW_HOST || "127.0.0.1" },
       config: { type: "string", short: "c", default: process.env.WORKFLOW_CONFIG_PATH || "workflows.yaml" },
       help: { type: "boolean", short: "h", default: false },
       daemon: { type: "boolean", short: "d", default: false },
