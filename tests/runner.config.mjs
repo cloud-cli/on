4f68fa59a8868cde8238ac843615fa3d06edd3a8
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-08-07 19:12:27 +0530

    chore: clean up

--- tests/runner.config.mjs
+++ tests/runner.config.mjs
@@ -1,15 +1,12 @@
-import { JsonFileReporter, SecretStore, SlackReporter } from '@cloud-cli/on';
+import { SecretStore, SlackReporter } from '@cloud-cli/on';
 
 // 1. Load secrets locally on host startup
 const secrets = new SecretStore('./.env');
 
 export default {
   database: process.env.SQLITE_HTTP_URL,
-  storagePath: '/tmp/workspaces',
+  storagePath: '/var/tmp/workspaces',
   reporters: [
-    new JsonFileReporter({
-      outputDir: '/tmp/reports',
-    }),
     new SlackReporter({
       token: secrets.get('SLACK_BOT_TOKEN'),
       channel: '#ci-cd-deployments',
