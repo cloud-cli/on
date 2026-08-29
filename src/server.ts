62ce54332c1476b6f883f00565e1fd0fc28f7eba
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-08-29 20:38:35 +0200

    fix: add meta for mobile screens

--- src/server.ts
+++ src/server.ts
@@ -256,10 +256,12 @@ export class WebhookServer {
       })
       .join('');
 
-    const html = `<!DOCTYPE html>
+    const html = `<!doctype html>
 <html lang="en" class="dark">
-<head>
-  <meta charset="UTF-8">
+  <head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
   <meta http-equiv="refresh" content="10"> <!-- Auto-refreshes every 10s -->
   <title>Workflow Engine Dashboard</title>
   <script src="https://cdn.tailwindcss.com"></script>
